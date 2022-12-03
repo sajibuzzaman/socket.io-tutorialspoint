d8220ef97ac3bc76822123c9e4f32b109843faef
Sajib Uzzaman <muhammadsajibuzzaman1998@gmail.com>
2022-12-03 14:57:09 +0600

    custom event

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/./index.html");
- });
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/./index.html");
+});
 
- //Whenever someone connects this gets executed
-io.on('connection', function(socket){
-    console.log('A user connected');
-    
-    setTimeout(function(){
-        socket.send('Sent a message 4seconds after connection!');
-     }, 4000);
-     
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-       console.log('A user disconnected');
+//Whenever someone connects this gets executed
+io.on("connection", function (socket) {
+  console.log("A user connected");
+
+  // // message event
+  // setTimeout(function(){
+  //     socket.send('Sent a message 4seconds after connection!');
+  //  }, 4000);
+
+  // Custom event
+  setTimeout(function () {
+    // Sending an object when emmiting an event
+    socket.emit("testerEvent", {
+      description: "A custom event named testerEvent!",
     });
- });
- 
+  }, 4000);
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
 http.listen(5000, () => {
-    console.log('Server is listening in port 5000')
-})
+  console.log("Server is listening in port 5000");
+});
